54482d79462b69df185ed9561b486e1a2b831023
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-20 13:15:47 -0400

    initial commit

---
+++ src/app/components/restaurant-card-list/restaurant-card-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RestaurantCardListComponent } from './restaurant-card-list.component';
+
+describe('RestaurantCardListComponent', () => {
+  let component: RestaurantCardListComponent;
+  let fixture: ComponentFixture<RestaurantCardListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RestaurantCardListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RestaurantCardListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/restaurant-card-list/restaurant-card-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-card-list',
+  templateUrl: './restaurant-card-list.component.html',
+  styleUrls: ['./restaurant-card-list.component.css']
+})
+export class RestaurantCardListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
