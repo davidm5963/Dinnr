a7dcfc56ff520159ba696d6bb072518ebb4baeac
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-20 13:12:43 -0400

    remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { LikeListComponent } from './components/like-list/like-list.component';
   declarations: [
     AppComponent,
     RestaurantCardComponent,
-    LikesListComponent,
     DislikeListComponent,
     LikeListComponent
   ],
