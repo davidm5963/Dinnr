778394858c4a45fbff158489afbb234092fe828d
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-20 14:41:13 -0400

    imports, create variable for api config data

--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Http, Headers } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
 
+  private API_KEY: string = environment.GOOGLE_API_KEY;
+  private API_URL: string = environment.GOOGLE_PLACES_API_URL;
+
   constructor() { }
+
+  getNearbyRestaurants(){
+
+  }
 }
