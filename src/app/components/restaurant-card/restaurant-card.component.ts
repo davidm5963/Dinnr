bf0107e1da1029fff0cc18fee00e2e53acfa9bcc
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-24 11:41:14 -0400

    mock images

--- src/app/components/restaurant-card/restaurant-card.component.ts
+++ src/app/components/restaurant-card/restaurant-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Restaurant } from '../../models/restaurant.model'
 import { MatCard } from "@angular/material/card"
+import { RestaurantService } from '../../services/restaurant.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-restaurant-card',
@@ -10,11 +12,17 @@ import { MatCard } from "@angular/material/card"
 export class RestaurantCardComponent implements OnInit {
 
   @Input() restaurant: Restaurant;
+  images: any[];
 
-
-  constructor() { }
+  constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit(restaurant = this.restaurant) {
+    //this.restaurantService.getRestaurantImages(this.restaurant).subscribe(data => {
+     // this.images = data;
+      this.images = ['https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1519844002%2Ffast-food-mobile-apps-chick-fil-a-FT-BLOG0218.jpg%3Fitok%3D7d_gu0JA&w=700&q=85',
+                     'https://www.healthline.com/hlcmsresource/images/News/food-fads/070615_restaurants_THUMB_LARGE.jpg']
+      console.log(this.images);
+    //});
   }
 
 }
